043adff9f7879ac5ed321ee9bb5548b5c340de2a
Knochen-Herr <137662063+Knochen-Herr@users.noreply.github.com>
2026-08-26 02:52:05 +0000

    Added folder for Backend & added code framework

---
+++ Backend/script.js
@@ -0,0 +1,38 @@
+let currentTime = new Date();
+let genSensID = Math.round(Math.random()*1000)
+let deterSensIDALT = value+1;
+let inputSensTemp = 100;
+let inputSensName = "BLANK";
+let inputSensActivatedState = 0;
+let inputSensLocation = "";
+let genSensTime = currentTime;
+let genSensTimeALT = Date.now();
+
+
+function LoginUser() {
+    
+}
+function LoginAdmin() {
+
+}
+
+
+function CreateTempSens() {
+
+}
+function ConfigTempSens() {
+
+}
+
+
+function CreateUserCred() {
+
+}
+function EditUserCred() {
+
+}
+function KillUser() {
+
+}
+
+
